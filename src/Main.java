a1a6d24a93058d0a34fd2622d939110bbd929da7
Hanner Conde <hanner.condeo@unac.edu.co>
2024-02-20 19:01:23 -0500

    Ya corregí los errores y pulí todo

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,17 @@ public class Main {
 
         boolean continuarEnMenu = true;
         Scanner input = new Scanner(System.in);
-        ImpresoraArray impresoraArray = new ImpresoraArray();
 
         while(continuarEnMenu){
-            System.out.println(
-                    "Presione el número de la opción que desea hacer\n" +
+            System.out.print(
+                    "\n\nPresione el número de la opción que desea hacer\n" +
                             "1 - Registrar productos nacionales\n" +
                             "2 - Registrar productos importados\n" +
                             "3 - Listar productos nacionales\n" +
                             "4 - Listar productos importados\n" +
                             "5 - Buscar producto nacional\n" +
-                            "6 - Buscar producto importado"
+                            "6 - Buscar producto importado" +
+                            "\nIndique su opción:\t"
             );
             int option = input.nextInt();
             input.nextLine();
@@ -55,37 +55,49 @@ public class Main {
 
             else if (option == 3) {
                 ImpresoraArray.mostrarArray(listaProductosNacionales);
+                if(listaProductosImportados.size() == 0){
+                    System.out.println("No hay productos en la lista de nacionales");
+                }
             }
 
             else if (option == 4) {
                 ImpresoraArray.mostrarArray((listaProductosImportados));
+                if(listaProductosImportados.size() == 0){
+                    System.out.println("No hay productos en esta lista de importados");
+                }
             }
 
 
 
             else if (option == 5){
-                input.nextLine();
                 System.out.println("Ingresa el STRING nombre del producto nacional que puedes buscar:\t");
                 String nombreABuscar = input.nextLine();
+                boolean flag = false;
                 for (Producto producto: listaProductosNacionales){
                     if (Objects.equals(producto.nombre, nombreABuscar)){
                         System.out.printf("Sí, el producto %s está en la lista.", nombreABuscar);
-                    } else {
-                        System.out.printf("No, el producto %s no está en la lista.", nombreABuscar);
+                        flag = true;
+                        break;
                     }
+                }
+                if (flag == false){
+                    System.out.printf("No, el producto %s no está en la lista.", nombreABuscar);
                 }
             }
 
             else if (option == 6){
-                input.nextLine();
                 System.out.println("Ingresa el STRING nombre del producto nacional que puedes buscar:\t");
                 String nombreABuscar = input.nextLine();
+                boolean flag = false;
                 for (Producto producto: listaProductosImportados){
                     if (Objects.equals(producto.nombre, nombreABuscar)){
                         System.out.printf("Sí, el producto %s está en la lista.", nombreABuscar);
-                    } else {
-                        System.out.printf("No, el producto %s no está en la lista.", nombreABuscar);
+                        flag = true;
+                        break;
                     }
+                }
+                if (flag == false){
+                    System.out.printf("No, el producto %s no está en la lista.", nombreABuscar);
                 }
             }
 
@@ -99,9 +111,9 @@ public class Main {
 
     public static void validarChao(boolean continuarEnMenu){
         Scanner inpuuut = new Scanner(System.in);
-        System.out.println("\nProceso terminado. Presione 0 si desea finalizar o enter si quiere seguir");
+        System.out.print("\nProceso terminado. Presione 0 si desea finalizar o enter si quiere seguir\t");
         String salgo = inpuuut.nextLine();
-        if (salgo == "0"){
+        if (Objects.equals(salgo, "0")){
             continuarEnMenu = false;
         }
     }
